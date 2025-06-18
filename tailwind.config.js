b139034299ba37c6e17ac4254e3ed75fc654107e
d1lmurodvic <dildoraishmuhamedova4@gmail.com>
2025-06-18 22:45:19 +0500

    sdf

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,10 +1,20 @@
-/** @type {import('postcss-load-config').Config} */
-const config = {
-  plugins: {
-    tailwindcss: {},
-    autoprefixer: {},
-    '@tailwindcss/line-clamp': {}, // Подключение плагина
+// tailwind.config.js
+import daisyui from 'daisyui';
+
+export default {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+    "./app/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [
+    daisyui,
+    require('@tailwindcss/line-clamp'), 
+  ],
+  daisyui: {
+    themes: ["light", "synthwave"],
   },
 };
-
-export default config;
