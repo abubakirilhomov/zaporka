c87d319158a7ca742513a2b42fbe3e65842c7652
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-06-25 09:30:27 +0500

    asdsad

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-    images: {
-      domains: ['zaporka-backend.onrender.com'], // Add your API domain here
-    },
-    experimental: {
-    serverComponentsExternalPackages: [], // Исключает конфликты с внешними пакетами
+  reactStrictMode: true,
+  images: {
+    domains: ['zaporka-backend.onrender.com'],
   },
-  };
+  serverExternalPackages: [], // <--- сюда перенесено
+};
+
 export default nextConfig;
