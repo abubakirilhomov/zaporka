5e4ceb342040dcaba6afc17217ba7847b50092c9
abubakirilhomov <2ilhomovabubakir2@gmail.com>
2025-06-25 17:43:48 +0500

    addas

--- src/app/(shop)/ClientShell.jsx
+++ src/app/(shop)/ClientShell.jsx
@@ -19,7 +19,7 @@ export default function ClientShell({ children }) {
           className="py-3 bg-primary z-10 text-base-100 px-5 fixed right-0 md:top-[30%] top-[25%] flex items-center gap-1 rounded-l-2xl rounded-none"
         >
           <TiShoppingCart />
-            <span className="rounded text-secondary font-bold">{cartItems?.items?.length != 0 ? cartItems?.items?.length : ""}</span>
+            <span className="rounded text-warning font-bold">{cartItems?.items?.length != 0 ? cartItems?.items?.length : ""}</span>
         </Link>
       </main>
       <Footer />
